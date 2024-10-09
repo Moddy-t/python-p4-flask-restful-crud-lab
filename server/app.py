49b73bf8058ac9349a24535f758c148848e240ac
Moddy-t <amalembamodesta@gmail.com>
2024-10-09 17:07:35 +0300

    UD lab

--- server/app.py
+++ server/app.py
@@ -46,7 +46,21 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self,id):
+        plant =Plant.query.filter_by(id =id).first()
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+            db.session.commit()
 
+            return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id =id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response(jsonify({}),204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
